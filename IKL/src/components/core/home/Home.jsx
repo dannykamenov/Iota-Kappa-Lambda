434fe260cd2fdbeda740e00074c12741fade6366
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-19 14:42:07 +0200

    added widget

--- IKL/src/components/core/home/Home.jsx
+++ IKL/src/components/core/home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Home.css';
+import home from '../../../assets/HOME.jpg';
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className='section1-box'>
+            <div className='our-mission'></div>
+            <div className='calendar-box'>
+                <div className="elfsight-app-7f5905de-8f48-4964-8149-c1826666edc7 widget" data-elfsight-app-lazy></div>
+            </div>
+        </div>
     )
 }
 

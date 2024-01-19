610cc2974ede8168e57574a3e988627d9c09893c
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-19 11:46:13 +0200

    centered nav and styled

--- IKL/src/components/header/Header.jsx
+++ IKL/src/components/header/Header.jsx
@@ -4,17 +4,33 @@ import logo from '../../assets/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 
+const linkStyle = {
+    color: '#ffffff',
+    textDecoration: 'none',
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    fontFamily: 'Roboto',
+    margin: '0 1rem',
+    padding: '0.5rem 0',
+    cursor: 'pointer',
+    transition: 'all 0.3s ease-in-out',
+    '&:hover': {
+        color: '#938807',
+    },
+    alignItems: 'center',
+};
+
 const Header = () => {
     return (
         <nav className='navbar'>
             <ul className='header list'>
-            <li className='header home'><Link to="/">HOME</Link></li>
-            <li className='header contact'><Link to="contact-us">CONTACT US</Link></li>
-            <li className='header leadership'><Link to="leadership">LEADERSHIP</Link></li>
+            <li className='header home link'><Link to="/" style={linkStyle}>HOME</Link></li>
+            <li className='header contact link'><Link to="contact-us" style={linkStyle}>CONTACT US</Link></li>
+            <li className='header leadership link'><Link to="leadership" style={linkStyle}>LEADERSHIP</Link></li>
             <li className='header logo'><Link to="/"><img src={logo} alt="Logo" className='logo-img' /></Link></li>
-            <li className='header events'><Link to="events-and-photos">EVENT & PHOTOS</Link></li>
-            <li className='header help'><Link to="help">HELPFUL LINKS</Link></li>
-            <li className='header profile'><Link to="profile"><FontAwesomeIcon icon={faUser} style={{color: "#ffffff",}} /></Link></li>
+            <li className='header events link'><Link to="events-and-photos" style={linkStyle}>EVENT & PHOTOS</Link></li>
+            <li className='header help link'><Link to="help" style={linkStyle}>HELPFUL LINKS</Link></li>
+            <li className='header profile link'><Link to="profile" style={linkStyle}><FontAwesomeIcon icon={faUser} style={{color: "#ffffff",}} /></Link></li>
             </ul>
         </nav>
     )

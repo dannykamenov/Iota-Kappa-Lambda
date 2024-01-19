bca22bff8a55d7c13e217b2ea3d5038105b17cbe
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-19 12:30:20 +0200

    added home component

--- IKL/src/App.jsx
+++ IKL/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/core/header/Header'
+import Home from './components/core/home/Home'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<h1>About</h1>} />
         <Route path="/contact" element={<h1>Contact</h1>} />
       </Routes>

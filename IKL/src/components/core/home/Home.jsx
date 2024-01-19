686cef1b969b80945de013bb29a2e63c845f5350
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-19 15:54:59 +0200

    added middle div slogan

--- IKL/src/components/core/home/Home.jsx
+++ IKL/src/components/core/home/Home.jsx
@@ -4,21 +4,22 @@ import home from '../../../assets/HOME.jpg';
 
 const Home = () => {
     return (
+    <>
         <div className='section1-box'>
             <div className='our-mission'>
                 <h1 className='our-mission-title'>Our Mission</h1>
                 <p className='our-mission-text'>The success of our effort to uplift the African American male 
-                is the civil rights issue of this generation and has nothing to do with anyone else but us,  
+                is the civil rights issue of this generation and has nothing to do with anyone else but us,
                 for it is our job. Mothers, grandmothers, teachers, caretakers, incarcerated parents and even 
                 the boys themselves have reached out to Alpha men for help. In order to be men, our boys must 
-                see men...Alpha Men! <br /> <br /> <strong>- Brother Mark S. Tillman, 34th General President, Alpha Phi Alpha</strong>
-
-</p>
+                see men...Alpha Men! <br /> <br /> <strong>- Brother Mark S. Tillman, 34th General President, Alpha Phi Alpha</strong></p>
             </div>
             <div className='calendar-box'>
                 <div className="elfsight-app-7f5905de-8f48-4964-8149-c1826666edc7 widget" data-elfsight-app-lazy></div>
             </div>
         </div>
+        <div className='slogan-div'><h1 className='slogan-text'>MANLY DEEDS, SCHOLARSHIP <br /> AND LOVE FOR ALL MANKIND</h1></div>
+    </>
     )
 }
 

c3fb050ad3cf022be066550741dbc48caf16b739
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-19 11:07:18 +0200

    Added logo

--- IKL/src/App.jsx
+++ IKL/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/about" element={<h1>About</h1>} />
--- IKL/src/components/header/Header.jsx
+++ IKL/src/components/header/Header.jsx
@@ -1,8 +1,21 @@
 import './Header.css'
 import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png'
 
 const Header = () => {
-    
+    return (
+        <nav>
+            <ul>
+            <li><Link to="/">HOME</Link></li>
+            <li><Link to="about">CONTACT US</Link></li>
+            <li><Link to="contact">LEADERSHIP</Link></li>
+            <li><Link to="/"><img src={logo} alt="Logo" /></Link></li>
+            <li><Link to="about">EVENT & PHOTOS</Link></li>
+            <li><Link to="contact">HELPFUL LINKS</Link></li>
+            <li><Link to="contact"><i></i></Link></li>
+            </ul>
+        </nav>
+    )
 };
 
 export default Header;

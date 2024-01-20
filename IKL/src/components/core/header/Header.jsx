898d5e5667c1818ce3632f3066ee1652bd282743
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-20 18:58:48 +0200

    header fix

--- IKL/src/components/core/header/Header.jsx
+++ IKL/src/components/core/header/Header.jsx
@@ -52,13 +52,19 @@ const Header = () => {
     return (
         <header className='navbar'>
             <ul className={headerClasses}>
-            <li className='header home link'><Link to="/" style={linkStyle} className='link-comp'>HOME</Link></li>
-            <li className='header contact link'><Link to="contact-us" style={linkStyle} className='link-comp'>CONTACT US</Link></li>
-            <li className='header leadership link'><Link to="leadership" style={linkStyle} className='link-comp'>LEADERSHIP</Link></li>
-            <li className='header logo'><Link to="/" style={logoStyle}><img src={logo} alt="Logo" className='logo-img' /></Link></li>
-            <li className='header events link'><Link to="events-and-photos" style={linkStyle} className='link-comp'>EVENT & PHOTOS</Link></li>
-            <li className='header help link'><Link to="help" style={linkStyle} className='link-comp'>HELPFUL LINKS</Link></li>
-            <li className='header profile link'><Link to="profile" style={linkStyle} ><FontAwesomeIcon icon={faUser} style={linkStyle} className='link-comp'/></Link></li>
+                <div className="left-links split">
+                    <li className='header home link'><Link to="/" style={linkStyle} className='link-comp'>HOME</Link></li>
+                    <li className='header contact link'><Link to="contact-us" style={linkStyle} className='link-comp'>CONTACT US</Link></li>
+                    <li className='header leadership link'><Link to="leadership" style={linkStyle} className='link-comp'>LEADERSHIP</Link></li>
+                </div>
+                <div className="logo-div">
+                    <li className='header logo'><Link to="/" style={logoStyle}><img src={logo} alt="Logo" className='logo-img' /></Link></li>
+                </div>
+                <div className="right-links split">
+                    <li className='header events link'><Link to="events-and-photos" style={linkStyle} className='link-comp'>EVENT & PHOTOS</Link></li>
+                    <li className='header help link'><Link to="help" style={linkStyle} className='link-comp'>HELPFUL LINKS</Link></li>
+                    <li className='header profile link'><Link to="profile" style={linkStyle} ><FontAwesomeIcon icon={faUser} style={linkStyle} className='link-comp'/></Link></li>
+                </div>
             </ul>
         </header>
     )

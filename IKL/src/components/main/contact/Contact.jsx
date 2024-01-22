c80d8547989a458d0cce1b26decd1eb83132b73f
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-22 11:50:57 +0200

    adding contact component

--- IKL/src/components/main/contact/Contact.jsx
+++ IKL/src/components/main/contact/Contact.jsx
@@ -1,5 +1,17 @@
-import './Contact.css'
+import "./Contact.css";
 
-const Contact = () => {};
+const Contact = () => {
+  return (
+    <>
+      <div className="contact-image">
+        <h1 className="contact-title">Get in touch!</h1>
+      </div>
+      <div className="contact-section">
+        <div className="contact-images"></div>
+        <div className="contact-form"></div>
+      </div>
+    </>
+  );
+};
 
 export default Contact;

d8e1afc972e88541984bf918507ea1675b7703ab
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-22 19:40:57 +0200

    added contact form

--- IKL/src/components/main/contact/Contact.jsx
+++ IKL/src/components/main/contact/Contact.jsx
@@ -1,7 +1,25 @@
 import "./Contact.css";
-import founders from '../../../assets/Founders.png'
+import founders from "../../../assets/Founders.png";
+import { useState } from "react";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    description: "",
+    membershipInterest: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here (e.g., send data to an API)
+    console.log(formData);
+  };
+
   return (
     <>
       <div className="contact-image">
@@ -9,10 +27,76 @@ const Contact = () => {
       </div>
       <div className="contact-section">
         <div className="contact-images">
-            <img src={founders} alt="" className="contact-photo1" />
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5831.355979647195!2d-76.15574605266613!3d43.048206224981165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d9f3b9413c46c7%3A0x3aa758c7ec4265ac!2sDowntown%2C%20Syracuse%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbg!4v1705921588946!5m2!1sen!2sbg" loading="lazy" className="google-maps"></iframe>
+          <img src={founders} alt="" className="contact-photo1" />
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5831.355979647195!2d-76.15574605266613!3d43.048206224981165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d9f3b9413c46c7%3A0x3aa758c7ec4265ac!2sDowntown%2C%20Syracuse%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbg!4v1705921588946!5m2!1sen!2sbg"
+            loading="lazy"
+            className="google-maps"
+          ></iframe>
         </div>
-        <div className="contact-form"></div>
+        <div className="contact-form">
+          <form onSubmit={handleSubmit} className="form-details">
+            <div className="name-div">
+              <p className="form-label">Name:</p>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="name-input"
+              />
+            </div>
+            <div className="email-div">
+              <p className="form-label">Email:</p>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="email-input"
+              />
+            </div>
+            <div className="desc-div">
+              <p className="form-label">Description:</p>
+              <textarea
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+                className="description-input"
+              />
+            </div>
+            <div className="radio-main-div">
+              <p className="form-label">Are you interested in a membership?</p>
+              <div className="radio-div">
+                <div className="radio-content">
+                  <input
+                    type="radio"
+                    name="membershipInterest"
+                    value="yes"
+                    checked={formData.membershipInterest === "yes"}
+                    onChange={handleChange}
+                    className="radio-input"
+                  />
+                  <p className="form-label">Yes</p>
+                </div>
+                <div className="radio-content">
+                  <input
+                    type="radio"
+                    name="membershipInterest"
+                    value="no"
+                    checked={formData.membershipInterest === "no"}
+                    onChange={handleChange}
+                    className="radio-input"
+                  />
+                  <p className="form-label">No</p>
+                </div>
+              </div>
+            </div>
+            <button type="submit" className="submit-btn-form">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );

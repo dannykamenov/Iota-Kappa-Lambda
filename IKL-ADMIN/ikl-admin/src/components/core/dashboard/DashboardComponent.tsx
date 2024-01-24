9d977eb5914d372e1d60e6908ccd4e8ebb5574ee
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-24 20:17:00 +0200

    added calendar button

--- IKL-ADMIN/ikl-admin/src/components/core/dashboard/DashboardComponent.tsx
+++ IKL-ADMIN/ikl-admin/src/components/core/dashboard/DashboardComponent.tsx
@@ -36,13 +36,13 @@ const DashboardComponent = () => {
         <div className="p-5 mx-auto block">
           <Label>Event Date</Label>
           <Popover>
-            <PopoverTrigger>
+            <PopoverTrigger asChild className="w-full">
               <Button
                 variant="outline"
-                className="w-[240px] pl-3 text-left font-normal"
+                className="pl-3 text-left font-normal"
               >
-                <CalendarIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <span className="sr-only">Toggle calendar</span>
+                <CalendarIcon/>
+                <span className="pl-2">Toggle calendar</span>
               </Button>
             </PopoverTrigger>
             <PopoverContent>

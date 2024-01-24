ff1929aca7d61152d8e699252815abd284e04ca7
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-24 21:39:19 +0200

    added header

--- IKL-ADMIN/ikl-admin/src/App.tsx
+++ IKL-ADMIN/ikl-admin/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/core/dashboard/Dashboard";
 import { ThemeProvider } from "./components/theme-provider";
+import Header from "./components/core/header/Header";
 
 function App() {
   return (
     <>
       <ThemeProvider>
         <Router>
+          <Header />
           <Routes>
             <Route path="/" element={<Dashboard />} />
           </Routes>
---
+++ IKL-ADMIN/ikl-admin/src/components/core/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {ModeToggle} from "../../mode-toggle";
+
+const Header = () => {
+  return (
+    <div className=" my-5 mx-auto w-2/5 text-end ">
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default Header;

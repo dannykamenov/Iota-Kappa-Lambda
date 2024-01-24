21baeb3e9d86a865d72100f56c147bfaaad5cc2d
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-24 21:17:21 +0200

    working on file upload

--- IKL-ADMIN/ikl-admin/src/components/core/dashboard/DashboardComponent.tsx
+++ IKL-ADMIN/ikl-admin/src/components/core/dashboard/DashboardComponent.tsx
@@ -12,36 +12,36 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+import "./Dashboard.css";
 
 const DashboardComponent = () => {
   const [formData, setFormData] = useState({});
   const [date, setDate] = React.useState<Date | undefined>(new Date());
+  const [file, setFile] = React.useState<File | null>(null);
 
   const handleForm = (e: any) => {
     e.preventDefault();
     console.log("Form submitted");
   };
 
-  React.useEffect(() => {
-    console.log(date);
-  }, [date]);
-
   return (
     <>
       <form onSubmit={handleForm} className="rounded-md border">
-        <div className="p-5">
+        <div className="p-5 px-5 pb-2">
           <Label>Title</Label>
           <Input placeholder="Title" type="text" />
         </div>
-        <div className="p-5 mx-auto block">
+        <div className="p-2 px-5">
+          <Label>Summary</Label>
+          <Input placeholder="Quick Summary" type="text" />
+        </div>
+        <div className="p-2 px-5 mx-auto block">
           <Label>Event Date</Label>
           <Popover>
             <PopoverTrigger asChild className="w-full">
-              <Button
-                variant="outline"
-                className="pl-3 text-left font-normal"
-              >
-                <CalendarIcon/>
+              <Button variant="outline" className="pl-3 text-left font-normal">
+                <CalendarIcon />
                 <span className="pl-2">Toggle calendar</span>
               </Button>
             </PopoverTrigger>
@@ -49,6 +49,32 @@ const DashboardComponent = () => {
               <Calendar mode="single" selected={date} onSelect={setDate} />
             </PopoverContent>
           </Popover>
+        </div>
+        <div className="p-2 px-5">
+          <Label>Event Description</Label>
+          <Textarea
+            placeholder="Describe the event here..."
+            maxLength={500}
+            className=" resize-none "
+          />
+        </div>
+        <div className="p-5 px-5 pb-2 grid max-w-sm items-center gap-1.5 justify-center mx-auto">
+          <Label htmlFor="mainImg" className="custom-file-upload">
+            Upload Main Image
+          </Label>
+          <Input
+            placeholder="Title"
+            type="file"
+            id="mainImg"
+            className="file-upload-btn"
+            onChange={(e) => {
+              if (e.target.files && e.target.files.length > 0) {
+                setFile(e.target.files[0]);
+              } else {
+                setFile(null);
+              }
+            }}
+          />
         </div>
       </form>
     </>

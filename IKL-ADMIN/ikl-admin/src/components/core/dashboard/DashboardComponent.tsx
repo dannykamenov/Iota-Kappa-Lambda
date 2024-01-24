a156ba2663d5fa234273dbeb04158e578ac9e9e1
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-24 21:29:56 +0200

    finalized event form ui

--- IKL-ADMIN/ikl-admin/src/components/core/dashboard/DashboardComponent.tsx
+++ IKL-ADMIN/ikl-admin/src/components/core/dashboard/DashboardComponent.tsx
@@ -19,6 +19,7 @@ const DashboardComponent = () => {
   const [formData, setFormData] = useState({});
   const [date, setDate] = React.useState<Date | undefined>(new Date());
   const [file, setFile] = React.useState<File | null>(null);
+  const [files, setFiles] = React.useState<File[] | null>(null);
 
   const handleForm = (e: any) => {
     e.preventDefault();
@@ -75,6 +76,30 @@ const DashboardComponent = () => {
               }
             }}
           />
+        </div>
+        <div className="p-5 px-5 pb-2 grid max-w-sm items-center gap-1.5 justify-center mx-auto">
+          <Label htmlFor="imgLib" className="custom-file-upload">
+            Upload Image Library
+          </Label>
+          <Input
+            placeholder="Title"
+            type="file"
+            id="imgLib"
+            className="file-upload-btn2"
+            onChange={(e) => {
+                if (e.target.files && e.target.files.length > 0) {
+                    setFiles(Array.from(e.target.files));
+                } else {
+                    setFiles(null);
+              }
+            }}
+            multiple
+          />
+        </div>
+        <div className="p-2 px-5 text-center">
+            <Button type="submit">
+                Submit
+            </Button>
         </div>
       </form>
     </>

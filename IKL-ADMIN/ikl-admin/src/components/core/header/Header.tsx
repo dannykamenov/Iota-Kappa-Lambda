766c101475f1a0a74374f53a7777fcaaade6246a
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-25 12:17:07 +0200

    adding navbar

--- IKL-ADMIN/ikl-admin/src/components/core/header/Header.tsx
+++ IKL-ADMIN/ikl-admin/src/components/core/header/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import {ModeToggle} from "../../mode-toggle";
+import { ModeToggle } from "../../mode-toggle";
+import { NavigationMenuDemo } from "./NavMenuDemo";
 
 const Header = () => {
   return (
-    <div className=" my-5 mx-auto w-2/5 text-end ">
+    <div className=" my-5 mx-auto w-2/5  flex justify-between">
+      <NavigationMenuDemo />
       <ModeToggle />
     </div>
   );

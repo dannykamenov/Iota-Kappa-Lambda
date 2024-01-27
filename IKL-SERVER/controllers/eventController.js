c209bea299d011b0ec6b02dc644eb0cdc6cf4af8
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-27 10:44:37 +0200

    added new controller

--- IKL-SERVER/controllers/eventController.js
+++ IKL-SERVER/controllers/eventController.js
@@ -18,6 +18,25 @@ async function createEvent(req, res) {
     }
 }
 
+async function getAllEvents(req, res) {
+    try {
+        const events = await Event.find();
+        res.status(200).json({
+            status: 'success',
+            results: events.length,
+            data: {
+                events
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
+}
+
 module.exports = {
-    createEvent
+    createEvent,
+    getAllEvents
 };
--- IKL-SERVER/router/router.js
+++ IKL-SERVER/router/router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { createEvent } = require('../controllers/eventController');
+const { createEvent, getAllEvents } = require('../controllers/eventController');
 
 router.post('/events', createEvent);
+router.get('/events', getAllEvents);
 
 module.exports = router;

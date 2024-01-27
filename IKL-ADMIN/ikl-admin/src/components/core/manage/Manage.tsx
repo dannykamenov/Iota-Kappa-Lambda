764822c6c748af5b7b18c47fcf43354a12bd2257
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-27 12:46:47 +0200

    finalized event list ui

--- IKL-ADMIN/ikl-admin/src/components/core/manage/Manage.tsx
+++ IKL-ADMIN/ikl-admin/src/components/core/manage/Manage.tsx
@@ -16,24 +16,31 @@ import {
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
 
-const ManageComponent = () => {
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
-    const [events, setEvents] = useState<any[]>([]);
+const ManageComponent = () => {
+  const [events, setEvents] = useState<any[]>([]);
 
   useEffect(() => {
     getEvents().then((res) => {
       setEvents(res.data.events);
-      console.log(events)
+      console.log(events);
     });
   }, []);
 
   return (
-    <div className=" w-1/2 mx-auto ">
+    <div className=" w-1/2 mx-auto">
       {events.map((event) => {
         return (
           <HoverCard>
             <HoverCardTrigger>
-              <Card className={event._id}>
+              <Card id={event._id} className="my-10">
                 <CardHeader>
                   <CardTitle>{event.title}</CardTitle>
                 </CardHeader>
@@ -42,6 +49,40 @@ const ManageComponent = () => {
                 </CardContent>
                 <CardContent>
                   <CardDescription>{event.description}</CardDescription>
+                </CardContent>
+                <CardContent className="mx-auto">
+                <strong>Main Image</strong>
+                  <CardDescription className="mx-auto">
+                    <img src={event.mainImg} alt="" className="mx-auto rounded-md border-5 border-black"/>
+                  </CardDescription>
+                </CardContent>
+                <CardContent>
+                <strong>Image Library</strong>
+                  <Carousel
+                    opts={{
+                      align: "start",
+                    }}
+                    className="w-full max-w-sm mx-auto"
+                  >
+                    <CarouselContent>
+                      {event.images.map((image:any) => (
+                        <CarouselItem
+                          className="medium:basis-1/2 large:basis-1/3"
+                        >
+                          <div className="p-1">
+                            <Card>
+                              <CardContent className="flex aspect-square items-center justify-center p-0">
+                                <img src={image} className="w-full h-full rounded-md border-5 border-black  object-cover">
+                                </img>
+                              </CardContent>
+                            </Card>
+                          </div>
+                        </CarouselItem>
+                      ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                  </Carousel>
                 </CardContent>
                 <CardFooter>
                   <span className="text-sm text-gray-400">

d49597c189c741e466ba19acefa2735630a5e30d
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-27 22:08:18 +0200

    added edit event

--- IKL-SERVER/controllers/eventController.js
+++ IKL-SERVER/controllers/eventController.js
@@ -1,58 +1,94 @@
-const mongoose = require('mongoose');
-const Event = require('../models/eventModel');
+const mongoose = require("mongoose");
+const Event = require("../models/eventModel");
 
 async function createEvent(req, res) {
-    try {
-        const newEvent = await Event.create(req.body);
-        res.status(201).json({
-            status: 'success',
-            data: {
-                event: newEvent
-            }
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err.message
-        });
-    }
+  try {
+    const newEvent = await Event.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        event: newEvent,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 }
 
 async function getAllEvents(req, res) {
-    try {
-        const events = await Event.find();
-        res.status(200).json({
-            status: 'success',
-            results: events.length,
-            data: {
-                events
-            }
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err.message
-        });
-    }
+  try {
+    const events = await Event.find();
+    res.status(200).json({
+      status: "success",
+      results: events.length,
+      data: {
+        events,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 }
 
 async function deleteEvent(req, res) {
-    try {
-        await Event.findByIdAndDelete(req.params.id);
-        res.status(204).json({
-            status: 'success',
-            data: null
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err.message
-        });
-    }
+  try {
+    await Event.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+}
+
+async function getEvent(req, res) {
+  try {
+    const event = await Event.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        event,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+}
+
+async function editEvent(req, res) {
+  try {
+    const event = await Event.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({
+      status: "success",
+      data: {
+        event,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 }
 
 module.exports = {
-    createEvent,
-    getAllEvents,
-    deleteEvent
+  createEvent,
+  getAllEvents,
+  deleteEvent,
+  getEvent,
+  editEvent,
 };
--- IKL-SERVER/router/router.js
+++ IKL-SERVER/router/router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { createEvent, getAllEvents, deleteEvent } = require('../controllers/eventController');
+const { createEvent, getAllEvents, deleteEvent, getEvent, editEvent } = require('../controllers/eventController');
 
 router.post('/events', createEvent);
 router.get('/events', getAllEvents);
 router.delete('/events/:id', deleteEvent);
+router.get('/events/:id', getEvent);
+router.put('/events/:id', editEvent);
 
 module.exports = router;

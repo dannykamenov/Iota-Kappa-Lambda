00720c785b4f206162cef8a75fe53de7dee36aaa
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-27 22:16:18 +0200

    Update EditComponent.tsx

--- IKL-ADMIN/ikl-admin/src/components/core/manage/EditComponent.tsx
+++ IKL-ADMIN/ikl-admin/src/components/core/manage/EditComponent.tsx
@@ -165,93 +165,104 @@ const EditComponent = () => {
 
   return (
     <>
-      <form onSubmit={handleForm} className="rounded-md border">
-        <div className="p-5 px-5 pb-2">
-          <Label>Title</Label>
-          <Input
-            placeholder="Title"
-            type="text"
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-            value={title}
-          />
-        </div>
-        <div className="p-2 px-5">
-          <Label>Summary</Label>
-          <Input
-            placeholder="Quick Summary"
-            type="text"
-            onChange={(e) => {
-              setSummary(e.target.value);
-            }}
-            value={summary}
-          />
-        </div>
-        <div className="p-2 px-5 mx-auto block">
-          <Label>Event Date</Label>
-          <Popover>
-            <PopoverTrigger asChild className="w-full">
-              <Button variant="outline" className="pl-3 text-left font-normal">
-                <CalendarIcon />
-                <span className="pl-2">Toggle calendar</span>
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent>
-              <Calendar mode="single" selected={date} onSelect={setDate} />
-            </PopoverContent>
-          </Popover>
-        </div>
-        <div className="p-2 px-5">
-          <Label>Event Start Time</Label>
-          <Select onValueChange={setSelectedTime} value={selectedTime}>
-            <SelectTrigger className="">
-              <SelectValue placeholder="Pick a time" className="time-select" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="1 AM">1:00 AM</SelectItem>
-              <SelectItem value="2 AM">2:00 AM</SelectItem>
-              <SelectItem value="3 AM">3:00 AM</SelectItem>
-              <SelectItem value="4 AM">4:00 AM</SelectItem>
-              <SelectItem value="5 AM">5:00 AM</SelectItem>
-              <SelectItem value="6 AM">6:00 AM</SelectItem>
-              <SelectItem value="7 AM">7:00 AM</SelectItem>
-              <SelectItem value="8 AM">8:00 AM</SelectItem>
-              <SelectItem value="9 AM">9:00 AM</SelectItem>
-              <SelectItem value="10 AM">10:00 AM</SelectItem>
-              <SelectItem value="11 AM">11:00 AM</SelectItem>
-              <SelectItem value="12 PM">12:00 PM</SelectItem>
-              <SelectItem value="1 PM">1:00 PM</SelectItem>
-              <SelectItem value="2 PM">2:00 PM</SelectItem>
-              <SelectItem value="3 PM">3:00 PM</SelectItem>
-              <SelectItem value="4 PM">4:00 PM</SelectItem>
-              <SelectItem value="5 PM">5:00 PM</SelectItem>
-              <SelectItem value="6 PM">6:00 PM</SelectItem>
-              <SelectItem value="7 PM">7:00 PM</SelectItem>
-              <SelectItem value="8 PM">8:00 PM</SelectItem>
-              <SelectItem value="9 PM">9:00 PM</SelectItem>
-              <SelectItem value="10 PM">10:00 PM</SelectItem>
-              <SelectItem value="11 PM">11:00 PM</SelectItem>
-              <SelectItem value="12 AM">12:00 AM</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="p-2 px-5">
-          <Label>Event Description</Label>
-          <Textarea
-            placeholder="Describe the event here..."
-            maxLength={500}
-            className=" resize-none "
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-            value={description}
-          />
-        </div>
-        <div className="p-2 px-5 text-center">
-          <Button type="submit">Submit</Button>
-        </div>
-      </form>
+      <h1 className="w-1/4 my-5 mx-auto p-10 text-4xl text-center font-bold small:w-full medium:w-3/4 xl:w-1/2 ">
+        Edit Event
+      </h1>
+      <div className=" w-1/3 my-5 mx-auto small:w-full medium:w-3/4 xl:w-1/2">
+        <form onSubmit={handleForm} className="rounded-md border">
+          <div className="p-5 px-5 pb-2">
+            <Label>Title</Label>
+            <Input
+              placeholder="Title"
+              type="text"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+              value={title}
+            />
+          </div>
+          <div className="p-2 px-5">
+            <Label>Summary</Label>
+            <Input
+              placeholder="Quick Summary"
+              type="text"
+              onChange={(e) => {
+                setSummary(e.target.value);
+              }}
+              value={summary}
+            />
+          </div>
+          <div className="p-2 px-5 mx-auto block">
+            <Label>Event Date</Label>
+            <Popover>
+              <PopoverTrigger asChild className="w-full">
+                <Button
+                  variant="outline"
+                  className="pl-3 text-left font-normal"
+                >
+                  <CalendarIcon />
+                  <span className="pl-2">Toggle calendar</span>
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Calendar mode="single" selected={date} onSelect={setDate} />
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div className="p-2 px-5">
+            <Label>Event Start Time</Label>
+            <Select onValueChange={setSelectedTime} value={selectedTime}>
+              <SelectTrigger className="">
+                <SelectValue
+                  placeholder="Pick a time"
+                  className="time-select"
+                />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="1 AM">1:00 AM</SelectItem>
+                <SelectItem value="2 AM">2:00 AM</SelectItem>
+                <SelectItem value="3 AM">3:00 AM</SelectItem>
+                <SelectItem value="4 AM">4:00 AM</SelectItem>
+                <SelectItem value="5 AM">5:00 AM</SelectItem>
+                <SelectItem value="6 AM">6:00 AM</SelectItem>
+                <SelectItem value="7 AM">7:00 AM</SelectItem>
+                <SelectItem value="8 AM">8:00 AM</SelectItem>
+                <SelectItem value="9 AM">9:00 AM</SelectItem>
+                <SelectItem value="10 AM">10:00 AM</SelectItem>
+                <SelectItem value="11 AM">11:00 AM</SelectItem>
+                <SelectItem value="12 PM">12:00 PM</SelectItem>
+                <SelectItem value="1 PM">1:00 PM</SelectItem>
+                <SelectItem value="2 PM">2:00 PM</SelectItem>
+                <SelectItem value="3 PM">3:00 PM</SelectItem>
+                <SelectItem value="4 PM">4:00 PM</SelectItem>
+                <SelectItem value="5 PM">5:00 PM</SelectItem>
+                <SelectItem value="6 PM">6:00 PM</SelectItem>
+                <SelectItem value="7 PM">7:00 PM</SelectItem>
+                <SelectItem value="8 PM">8:00 PM</SelectItem>
+                <SelectItem value="9 PM">9:00 PM</SelectItem>
+                <SelectItem value="10 PM">10:00 PM</SelectItem>
+                <SelectItem value="11 PM">11:00 PM</SelectItem>
+                <SelectItem value="12 AM">12:00 AM</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="p-2 px-5">
+            <Label>Event Description</Label>
+            <Textarea
+              placeholder="Describe the event here..."
+              maxLength={500}
+              className=" resize-none "
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+              value={description}
+            />
+          </div>
+          <div className="p-2 px-5 text-center">
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };

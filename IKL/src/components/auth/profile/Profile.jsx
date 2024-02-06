b74f0b031b7cbdbbf2c689ea99f53ff41287b6fd
Daniel Kamenov <sorostmcoc@gmail.com>
2024-02-06 19:14:09 +0200

    duplicate request fix

--- IKL/src/components/auth/profile/Profile.jsx
+++ IKL/src/components/auth/profile/Profile.jsx
@@ -1,13 +1,16 @@
 import './Profile.css';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
 import { createUser } from '@/components/api/userApi';
 
 const Profile = () => {
     const { user } = useKindeAuth();
+    const [processed, setProcessed] = useState(false);
+    const requestSentRef = useRef(false);
 
     useEffect(() => {
-        if (user) {
+        if (user && !requestSentRef.current) {
+            requestSentRef.current = true;
             const name = `${user.given_name} ${user.family_name}`
             const userData = {
                 name,
@@ -15,6 +18,7 @@ const Profile = () => {
                 role: 'user',
             };
             createUser(userData).then(response => {
+                setProcessed(true);
                 console.log(response.message);
             }).catch(err => {
                 console.error('Error creating user:', err);

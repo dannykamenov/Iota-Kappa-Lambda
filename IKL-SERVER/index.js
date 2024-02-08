41e2a2069954bf4ccc2d1dc79277327d06768f50
Daniel Kamenov <sorostmcoc@gmail.com>
2024-02-08 10:41:05 +0200

    cors fixes

--- IKL-SERVER/index.js
+++ IKL-SERVER/index.js
@@ -10,10 +10,6 @@ const router = require('./router/router');
 // Allow requests from any origin - adjust to your needs
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
 }));
 app.use(bodyParser.json());
 console.log(mongo_uri)

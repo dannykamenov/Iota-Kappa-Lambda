7f36bef11bd6fff896a9a739b50c571071121ff5
Daniel Kamenov <sorostmcoc@gmail.com>
2024-02-09 22:34:20 +0200

    server update

--- IKL-SERVER/index.js
+++ IKL-SERVER/index.js
@@ -6,22 +6,22 @@ const bodyParser = require('body-parser');
 const mongo_uri = process.env.DB_URI || 'mongodb://127.0.0.1:27017/ikl-events';
 const router = require('./router/router');
 
+const allowedOrigins = [
+  'http://localhost:5174',
+  'http://localhost:5173'
+]
 
-// Allow requests from any origin - adjust to your needs
 app.use(express.json());
 app.use(cors({
-  origin: '*',
+  origin: allowedOrigins,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
 }));
 app.use(bodyParser.json());
-console.log(mongo_uri)
-// Your other middleware and route setup goes here...
 mongoose.connect(`${mongo_uri}`);
 app.use('/api', router)
-// Start the server
-const port = 3000; // Replace with your desired port number
+const port = 3000; 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
